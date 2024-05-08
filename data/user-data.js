0dc3355867023bddd94bf2123328f2bce8ce2e73
Anand Kadale <95684382+Anandk008@users.noreply.github.com>
2024-05-08 15:17:58 +0530

    Update user-data.js

--- data/user-data.js
+++ data/user-data.js
@@ -7,7 +7,7 @@ export const userData = {
   geeksforGeeks: 'https://auth.geeksforgeeks.org/user/anandk008',
   codeChef: 'https://codechef.com/users/anand_008',
   leetcode: "https://leetcode.com/anand_008/",
-  resume: "https://drive.google.com/file/d/1GXKQoVyiLu8i-OX1fEhaai7j9K_6nW0t/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1GXKQoVyLu8i-OX1fE1haaI7j9K_6nW0t/view?usp=sharing",
   skills: ['C','C++','HTML', 'CSS','JavaScript', 'NodeJS', 'ReactJS', 'Git', 'GoLang', 'Python', 'Docker', 'Kubernetes'],
   timezone: '+5'
 };
